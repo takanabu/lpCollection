dd1c424bfde0a725815efbe30362382be552f1fc
takayama manabu <mt01work01@gmail.com>
2024-05-19 10:46:49 +0900

    jsdesign-1

---
+++ src/app/Http/Controllers/JsDesignController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class JsDesignController extends Controller
+{
+    public function index()
+    {
+        return view('JsDesign');
+    }
+}
+
