c53ed1d61ec891816fd26c3757e226b07f86e31b
takayama manabu <mt01work01@gmail.com>
2024-05-25 00:23:25 +0900

    Blog途中

---
+++ src/resources/views/Blog.blade.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>エンジニアブログ</title>
+    <link rel="stylesheet" href="{{ asset('css/Blog.css') }}">
+</head>
+<body>
+    <header>
+        <div class="wrapper">
+        <h2>エンジニアのブログ</h2>
+        </div>
+
+        <nav>
+            <ul class="wrapper">
+                <li><a href="#">新着</a></li>
+                <li><a href="#">コラム</a></li>
+                <li><a href="#">カテゴリ</a></li>
+                <li><a href="#">コンタクト</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <div class="new-article wrapper">
+        <article>
+            <img src="./img/blog/pc01.jpg" alt="">
+            <p>半年間６９万円のプログラミング教室受講料は高かった？安かった？</p>
+            <div><a href="#">もっと読む</a></div>
+        </article>
+        <article>
+            <img src="./img/blog/pc02.jpg" alt="">
+            <p>プログラミング未経験の４５歳がチャレンジしてみて思うこと</p>
+            <div><a href="#">もっと読む</a></div>
+        </article>
+        <article>
+            <img src="./img/blog/pc03.jpg" alt="">
+            <p>プログラミングで食べていくというのは短距離走ではなくマラソンだったのか？</p>
+            <div><a href="#">もっと読む</a></div>
+        </article>
+    </div>
+    <div class="container wrapper">
+        <main>
+            <article>
+                <h1>フロントエンドの最新技術</h1>
+                <ul>
+                    <li>2022/01/15</li>
+                    <li>カテゴリ</li>
+                </ul>
+                <img src="./img/blog/pc01.jpg" alt="">
+                <p>
+                    最新のフロントエンド技術について思うこと。
+                </p>
+                <div>
+                    <a href="#">もっと読む</a>
+                </div>
+            </article>
+            <article>
+                <h1>フロントエンドの最新技術</h1>
+                <ul>
+                    <li>2022/01/15</li>
+                    <li>カテゴリ</li>
+                </ul>
+                <img src="./img/blog/pc01.jpg" alt="">
+                <p>
+                    最新のフロントエンド技術について思うこと。
+                </p>
+                <div>
+                    <a href="#">もっと読む</a>
+                </div>
+            </article>
+            <article>
+                <h1>フロントエンドの最新技術</h1>
+                <ul>
+                    <li>2022/01/15</li>
+                    <li>カテゴリ</li>
+                </ul>
+                <img src="./img/blog/pc01.jpg" alt="">
+                <p>
+                    最新のフロントエンド技術について思うこと。
+                </p>
+                <div>
+                    <a href="#">もっと読む</a>
+                </div>
+            </article>
+        </main>
+    </div>
+</body>
+</html>
