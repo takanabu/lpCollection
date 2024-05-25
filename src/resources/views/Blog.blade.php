5e061381e03c52adeeabfb1e0b1f7c2233ff3621
takayama manabu <mt01work01@gmail.com>
2024-05-25 11:37:06 +0900

    Blog見た目完成

--- src/resources/views/Blog.blade.php
+++ src/resources/views/Blog.blade.php
@@ -25,12 +25,12 @@
     <div class="new-article wrapper">
         <article>
             <img src="./img/blog/pc01.jpg" alt="">
-            <p>半年間６９万円のプログラミング教室受講料は高かった？安かった？</p>
+            <p>半年間で６９万円のプログラミング教室の受講料は高かった？安かった？</p>
             <div><a href="#">もっと読む</a></div>
         </article>
         <article>
             <img src="./img/blog/pc02.jpg" alt="">
-            <p>プログラミング未経験の４５歳がチャレンジしてみて思うこと</p>
+            <p>プログラミング未経験の４５歳がプログラミング教室を卒業してその後は</p>
             <div><a href="#">もっと読む</a></div>
         </article>
         <article>
@@ -48,9 +48,11 @@
                     <li>カテゴリ</li>
                 </ul>
                 <img src="./img/blog/pc01.jpg" alt="">
-                <p>
+                <div class="article-intro">
+                    <p>
                     最新のフロントエンド技術について思うこと。
-                </p>
+                    </p>
+                </div>
                 <div>
                     <a href="#">もっと読む</a>
                 </div>
@@ -62,9 +64,11 @@
                     <li>カテゴリ</li>
                 </ul>
                 <img src="./img/blog/pc01.jpg" alt="">
-                <p>
+                <div class="article-intro">
+                    <p>
                     最新のフロントエンド技術について思うこと。
-                </p>
+                    </p>
+                </div>
                 <div>
                     <a href="#">もっと読む</a>
                 </div>
@@ -76,14 +80,59 @@
                     <li>カテゴリ</li>
                 </ul>
                 <img src="./img/blog/pc01.jpg" alt="">
-                <p>
+                <div class="article-intro">
+                    <p>
                     最新のフロントエンド技術について思うこと。
-                </p>
+                    </p>
+                </div>
                 <div>
                     <a href="#">もっと読む</a>
                 </div>
             </article>
         </main>
+        <aside>
+            <section class="author">
+                <img src="./img/blog/MabuGaku.png" alt="">
+                <h4>Mabu GAku</h4>
+                <p>
+                    コーチテックのプログラミング講座卒業後
+                </p>
+            </section>
+            <section class="ranking">
+                <h4>ランキング</h4>
+                <div class="ranking-article">
+                    <a href="#">
+                        <img src="./img/blog/pc01.jpg" alt="">
+                        <p>HTML/CSS入門</p>
+                    </a>
+                </div>
+                <div class="ranking-article">
+                    <a href="#">
+                        <img src="./img/blog/pc02.jpg" alt="">
+                        <p>Javascript入門</p>
+                    </a>
+                </div>
+                <div class="ranking-article">
+                    <a href="#">
+                        <img src="./img/blog/pc01.jpg" alt="">
+                        <p>Python入門</p>
+                    </a>
+                </div>
+            </section>
+            <section class="archive">
+                <h4>アーカイブ</h4>
+                <ul>
+                    <li><a href="#">2022/01/25(XX日)</a></li>
+                    <li><a href="#">2022/01/25(XX日)</a></li>
+                    <li><a href="#">2022/01/25(XX日)</a></li>
+                    <li><a href="#">2022/01/25(XX日)</a></li>
+                    <li><a href="#">2022/01/25(XX日)</a></li>
+                    <li><a href="#">2022/01/25(XX日)</a></li>
+                    <li><a href="#">2022/01/25(XX日)</a></li>
+                    <li><a href="#">2022/01/25(XX日)</a></li>
+                </ul>
+            </section>
+        </aside>
     </div>
 </body>
 </html>
