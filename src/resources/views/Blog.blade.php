9f003ff1af9970f220833923b40994834ff19dcd
takayama manabu <mt01work01@gmail.com>
2024-06-04 05:44:05 +0900

    blog.blade2

--- src/resources/views/Blog.blade.php
+++ src/resources/views/Blog.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>エンジニアブログ</title>
     <link rel="stylesheet" href="{{ asset('css/Blog.css') }}">
+    <link href="https://fonts.googleapis.com/css2?family=Zen+Kaku+Gothic+New:wght@300;400;500;700;900&display=swap" rel="stylesheet">
 </head>
 <body>
     <header>
