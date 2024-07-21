48685cfd27b3beb8b9c11237618937f0727a3442
takayama manabu <mt01work01@gmail.com>
2024-07-21 20:21:46 +0900

    かくとくごーるど

---
+++ src/resources/views/blog/header/gold.blade.php
@@ -0,0 +1,189 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+
+    <link rel="stylesheet" href="{{ asset('css/reset.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/gold.css') }}">
+    <title>かくとくゴールド</title>
+</head>
+<body>
+    <div id="header" class="header-type1 header animate">
+        <div class=layout>
+            <header class="header">
+                <h1 class="header-ttl">
+                    かくとくゴールド
+                </h1>
+                <nav class="header-nav">
+                    <ul class="header-nav-list">
+                        <li class="header-nav-item"><a href="/index"target="_blank">ほーむ</a>
+                        </li>
+                        <li><a href="/blog"target="_blank">ぼうけんのしょ</a>
+                        </li>
+                        <li><a href="/history"target="_blank">ぼうけんのりれき</a>
+                        </li>
+                        <li><a href="/level"target="_blank">レベルあげのりれき</a>
+                        </li>
+                        <li><a href="magic"target="_blank">おぼえたまほう</a>
+                        </li>
+                    </ul>
+                </nav>
+            </header>
+
+            <main class="main">
+                <article class="content">
+                    <div>
+                        <a href="">
+                            トータル獲得ゴールド
+                        </a>
+                        <div>
+                            <a href="">
+                                
+                            </a>
+                        </div>
+                    </div>
+                    <div>
+                        <p>
+                            使ったゴールド合計
+                        </p>
+                    </div>
+                    <div>
+                        <p>
+                            獲得ゴールド合計
+                        </p>
+                    </div>
+                    <table class="table">
+                        <tr class="table-title">
+                            <th class="period">
+                                <a class="period-title" href="">
+                                    年代
+                                </a>
+                            </th>
+                            <th class="action-title">
+                                <a href="">
+                                    行動
+                                </a>
+                            </th>
+                            <th class="consumption-title">
+                                <a href="">
+                                    消費ゴールド
+                                </a>
+                            </th>
+                        </tr>
+                        <tr class="table-title-part1">
+                            <td class="period-part1">
+                                <a href="">
+                                    ２０２３年　１０月
+                                </a>
+                            </td>
+                            <td class="action-part1">
+                                <a href="">
+                                    オンライン魔法(プログラミング)スクール入学
+                                </a>
+                            </td>
+                            <td class="consumption-part1">
+                                <a href="">
+                                    ６６万　ゴールド
+                                </a>
+                            </td>
+                        </tr>
+                        <tr class="table-title-part2">
+                            <td class="period-part2">
+                                <a href="">
+                                    ２０２４年　３月
+                                </a>
+                            </td>
+                            <td class="action-part2">
+                                <a href="">
+                                    オンライン魔法(プログラミング)スクール卒業
+                                </a>
+                            </td>
+                            <td class="consumption-part2">
+                                <a href=""></a>
+                            </td>
+                        </tr>
+                        <tr class="table-title-part3">
+                            <td class="period-part3">
+                                <a href="">
+                                    ２０２４年　４月
+                                </a>
+                            </td>
+                            <td class="action-part3">
+                                <a class="action-part3-text" href="">
+                                    副業特化型、魔法(プログラミング)スクールだったが、<br>
+                                    卒業後すぐに副業できるほどスキルは上がらず。<br>
+                                    本気で魔法使いを目指すなら、 <br>
+                                    魔法(プログラミング)系会社へ転職して <br>
+                                    実務経験を積む必要があるとスクール側から言われ<br>
+                                    転職を考える。
+                                </a>
+                            </td>
+                            <td class="consumption-part3">
+                                <a href=""></a>
+                            </td>
+                        </tr>
+                        <tr class="table-title-part4">
+                            <td class="period-part4">
+                                <a href="">
+                                    ２０２４年　４月
+                                </a>
+                            </td>
+                            <td class="action-part4">
+                                <a class="action-part4-text" href="">
+                                    とりあえずスクールで学んだ知識で、<br>
+                                    自分のサイトを作成し始める。
+                                </a>
+                            </td>
+                            <td class="consumption-part4">
+                                <a href=""></a>
+                            </td>
+                        </tr>
+                        <tr class="table-title-part5">
+                            <td class="period-part5">
+                                <a href="">
+                                    ２０２４年　４月 　　　　　～６月
+                                </a>
+                            </td>
+                            <td class="action-part5">
+                                <a class="action-part5-text" href="">
+                                    転職活動開始<br>
+                                    １３０社くらいトライするが、書類選考で通らず。<br>            ６社ほど面接まで行くも落とされる。
+                                </a>
+                            </td>
+                            <td class="consumption-part5">
+                                <a href=""></a>
+                            </td>
+                        </tr>
+                    </table>
+                </article>
+            </main>
+
+            <footer class="footer">
+                <div class="footer-inner">
+                    <div class="footer-ttl">
+                        <a>魔法使い(プログラマー)の冒険の書</a>
+                    </div>
+                    <nav class="footer-nav">
+                        <ul class="footer-nav-list">
+                            <li class="footer-nav-item">
+                                <a href="#">ホーム</a>
+                            </li>
+                            <li class="footer-nav-item">
+                                <a href="/privacy-policy" target="blank">プライバシーポリシー</a>
+                            </li>
+                            <li class="footer-nav-item">
+                                <a href="/contact" target="blank" >お問合せ</a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+                <small class="copyright">
+                    &copy; 2024 embrace your, inc
+                </small>
+            </footer>
+        </div>
+    </div>
+</body>
+</html>
